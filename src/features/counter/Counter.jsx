a9eef90c6a3911bc3d9a79ccad15a01ff62776fa
mjmon <>
2023-04-29 18:49:43 +0800

    increment by amount and incrementsync

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import {
+  increment,
+  decrement,
+  incrementAsync,
+  incrementByAmount,
+} from "./counterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="">
         <button className="" onClick={() => dispatch(increment())}>
           Increment
         </button>
         <span>{count}</span>
         <button className="" onClick={() => dispatch(decrement())}>
           Decrement
+        </button>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            dispatch(incrementByAmount(Number(incrementAmount)));
+          }}
+        >
+          Add Amount
+        </button>
+        <button
+          onClick={() => dispatch(incrementAsync(Number(incrementAmount)))}
+        >
+          Add Async
         </button>
       </div>
     </div>

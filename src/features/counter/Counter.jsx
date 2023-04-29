4fdb1fe66bcb3e89ea67ff673355779168079fa7
mjmon <>
2023-04-29 16:58:08 +0800

    setup redux and basic layout

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <button className="" onClick={() => dispatch(increment)}>
+        Increment
+      </button>
+      <span>{count}</span>
+      <button className="" onClick={() => dispatch(decrement)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
